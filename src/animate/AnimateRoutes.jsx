faae01faa7e555e2dfb6e38a9f24c44bbdc1d907
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2022-12-03 23:11:50 +0530

    framer motion animation

---
+++ src/animate/AnimateRoutes.jsx
@@ -0,0 +1,25 @@
+// import { Routes, Route, useLocation } from "react-router-dom";
+// import LandingPage from "../pages/LandingPage";
+// import Home from "../pages/Home";
+// import SignIn from "../pages/SignIn";
+// import Signup from "../pages/Signup";
+// import PageNotFound from "../pages/PageNotFound";
+
+// import { AnimatePresence } from "framer-motion";
+
+// const AnimateRoutes = () => {
+//   const location = useLocation();
+//   return (
+//     <AnimatePresence>
+//       <Routes location={location} key={location.pathname}>
+//         <Route path="/" element={<LandingPage />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/signin" element={<SignIn />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="*" element={<PageNotFound />} />
+//       </Routes>
+//     </AnimatePresence>
+//   );
+// };
+
+// export default AnimateRoutes;
